fix logic in payoff calculation

american.py
@@ -4,7 +4,7 @@ import xgboost as xgb
 from sklearn.neural_network import MLPRegressor
 
 # Uses machine learning for regression and optimal stopping
-def longstaff_schwartz (S0, r, sigma, T, K, n_trials, n_timesteps, option_type, ml_model):
+def longstaff_schwartz(S0, r, sigma, T, K, n_trials, n_timesteps, option_type, ml_model):
     S = np.zeros((n_trials, n_timesteps))
     S[:,0] = S0
     rng = np.random.default_rng(42)
@@ -19,17 +19,25 @@ def longstaff_schwartz (S0, r, sigma, T, K, n_trials, n_timesteps, option_type, 
 
     if option_type == 'call':
         payoffs[:,n_timesteps-1] = np.maximum(ST - K, 0)
-    if option_type == 'put':
+    elif option_type == 'put':
         payoffs[:,n_timesteps-1] = np.maximum(K - ST, 0)
 
     for t in range(n_timesteps-2,-1,-1):
-        itm_indices = np.where(S[:,t] > 0)
-        otm_indices = np.where(S[:,t] <= 0)
+        if option_type == 'call':
+            itm_indices = np.where(S[:,t] > K)
+            otm_indices = np.where(S[:,t] <= K)
+        elif option_type == 'put':
+            itm_indices = np.where(S[:,t] < K)
+            otm_indices = np.where(S[:,t] >= K)
+        if len(itm_indices[0]) == 0:
+            payoffs[:, t] = payoffs[:, t+1] * np.exp(-r*dt)
+            continue
         X = S[itm_indices, t].reshape(-1, 1)
         y = payoffs[itm_indices,t+1] * np.exp(-r*dt)
         if ml_model == "xgboost":
             model = xgb.XGBRegressor()
-        if ml_model == "mlp":
+            model.fit(X, y)
+        elif ml_model == "mlp":
             model = MLPRegressor(
                 hidden_layer_sizes=(64, 64),  
                 activation='relu',
@@ -37,11 +45,12 @@ def longstaff_schwartz (S0, r, sigma, T, K, n_trials, n_timesteps, option_type, 
                 max_iter=500,
                 random_state=42
             )
-        model.fit(X, y)
+            model.fit(X, y.ravel())
+        
         future_val = model.predict(X).flatten()
         if option_type == 'call':
             current_val = np.maximum(S[itm_indices, t] - K, 0).flatten()
-        if option_type == 'put':
+        elif option_type == 'put':
             current_val = np.maximum(K - S[itm_indices, t], 0).flatten()
 
         itm = itm_indices[0]  
@@ -49,8 +58,7 @@ def longstaff_schwartz (S0, r, sigma, T, K, n_trials, n_timesteps, option_type, 
 
         payoffs[itm[should_exercise], t] = current_val[should_exercise]
         payoffs[itm[~should_exercise], t] = payoffs[itm[~should_exercise], t+1] * np.exp(-r*dt)
-
-        payoffs[otm_indices, t] = payoffs[otm_indices, t+1]
+        payoffs[otm_indices, t] = payoffs[otm_indices, t+1] * np.exp(-r*dt)
 
 
     profit = np.mean(payoffs[:,0])
@@ -60,3 +68,17 @@ def longstaff_schwartz (S0, r, sigma, T, K, n_trials, n_timesteps, option_type, 
     upper = profit+1.96*SE
 
     return profit, lower, upper
+
+
+S0 = 100
+r = 0.05
+sigma = 1
+T = 1
+K = 50
+n_trials = 100
+n_timesteps = 100
+option_type = 'call'
+ml_model = 'mlp'
+
+profit, lower, upper = longstaff_schwartz(S0, r, sigma, T, K, n_trials, n_timesteps, option_type, ml_model)
+print(profit)

european.py
@@ -9,7 +9,7 @@ def monte_carlo(S0, r, sigma, T, K, N, option_type):
 
     if option_type == 'call':
         payoffs = np.maximum(ST - K, 0)
-    if option_type == 'put':
+    elif option_type == 'put':
         payoffs = np.maximum(K - ST, 0)
 
     profit = np.mean(payoffs) * np.exp(-r*T)
@@ -29,7 +29,7 @@ def monte_carlo_antithetic(S0, r, sigma, T, K, N, option_type):
     if option_type == 'call':
         p1 = np.maximum(ST1 - K, 0)
         p2 = np.maximum(ST2 - K, 0)
-    if option_type == 'put':
+    elif option_type == 'put':
         p1 = np.maximum(K - ST1, 0)
         p2 = np.maximum(K - ST2, 0)
 
@@ -52,6 +52,6 @@ def black_scholes(S0, r, sigma, T, K, option_type):
     if option_type == 'call':
         C = S0*norm.cdf(d1) - K*np.exp(-r*T)*norm.cdf(d2)
         return C
-    if option_type == 'put':
+    elif option_type == 'put':
         P = K*np.exp(-r*T)*norm.cdf(-d2) - S0*norm.cdf(-d1)
         return P